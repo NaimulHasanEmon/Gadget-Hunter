f1244c556a815f2fb8539aa4327017568579da86
emon12700 <hasan15-5601@diu.edu.bd>
2024-04-30 16:21:23 +0600

    price setup

--- Js/phone.js
+++ Js/phone.js
@@ -33,8 +33,9 @@ const displayPhones = (phones,isShowAll) => {
                     class="rounded-xl hover:scale-105 duration-100" />
             </figure>
             <div class="card-body items-center text-center">
-                <h2 class="card-title">${phones.phone_name}</h2>
-                <p>There are many variations of passages of available, but the majority have suffered</p>
+                <h2 class="card-title text-black">${phones.phone_name}</h2>
+                <p class="text-slate-600 text-2xl">Price: 55,000 Taka</p>
+                <p class="text-slate-600">There are many variations of passages of available, but the majority have suffered</p>
                 <div class="card-actions">
                     <button onclick = 'handleShowDetails("${phones.slug}"); show_details_modal.showModal()' class="btn btn-primary text-xl hover:scale-110 duration-350">Show Details</button>
                 </div>
